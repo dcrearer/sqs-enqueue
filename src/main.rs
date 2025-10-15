b6b1ea5cb8d0531e4e4d450cb33df46610d0a263
Dwayne Crearer <dcrearer@gmail.com>
2025-10-15 10:42:28 -0400

    removed MessageSystemAttribute - no immediate requirement to add X-Ray trace header string

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use aws_config::BehaviorVersion;
 use aws_sdk_sqs::{Client, config::Region};
 use sqs_enqueue::configuration::Args;
-use std::collections::HashMap;
+// use std::collections::HashMap;
 use aws_sdk_sqs::types::MessageAttributeValue;
-use aws_sdk_sqs::types::{MessageSystemAttributeNameForSends, MessageSystemAttributeValue};
-
+// use aws_sdk_sqs::types::{MessageSystemAttributeNameForSends, MessageSystemAttributeValue};
 
 
 #[tokio::main]
@@ -29,15 +28,6 @@ async fn main() -> Result<(), aws_sdk_sqs::Error> {
         .data_type("String")
         .build()?;
 
-    let mut system_attrs = HashMap::new();
-    system_attrs.insert(
-        MessageSystemAttributeNameForSends::AwsTraceHeader,
-        MessageSystemAttributeValue::builder()
-            .string_value("Root=1-5e1b4151-5ac6c58f4c84240da44aa5d8")
-            .data_type("String")
-            .build()?
-    );
-
     let client = Client::new(&config);
     let _ = client
         .send_message()
@@ -46,7 +36,6 @@ async fn main() -> Result<(), aws_sdk_sqs::Error> {
         .delay_seconds(args.delay)
         .message_group_id(args.id)
         .message_attributes("priority", attr)
-        .set_message_system_attributes(Some(system_attrs))
         .send()
         .await?;
 
